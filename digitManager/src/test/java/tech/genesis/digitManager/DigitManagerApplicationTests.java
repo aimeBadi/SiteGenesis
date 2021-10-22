a79d16a52696ec113be332dbde5577a80d89eb03
emmanuel <88566401+Mpunga@users.noreply.github.com>
2021-10-22 10:53:39 +0200

    Add files via upload

---
+++ digitManager/src/test/java/tech/genesis/digitManager/DigitManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.genesis.digitManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
